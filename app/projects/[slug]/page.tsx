67670ffbbf1c67bebadb2006a5f2ca1df9bad8dc
skhandel003 <skhandel003@gmail.com>
2025-05-09 23:32:41 -0400

    Remove Redis

--- app/projects/[slug]/page.tsx
+++ app/projects/[slug]/page.tsx
@@ -1,7 +1,7 @@
 import { notFound } from "next/navigation";
 import { allProjects } from "contentlayer/generated";
 import { Mdx } from "@/app/components/mdx";
-import { Header } from "./header";
+
 import "./mdx.css";
 import { ReportView } from "./view";
 import { Redis } from "@upstash/redis";
@@ -14,7 +14,7 @@ type Props = {
   };
 };
 
-const redis = Redis.fromEnv();
+//const redis = Redis.fromEnv();
 
 export async function generateStaticParams(): Promise<Props["params"][]> {
   return allProjects
@@ -25,24 +25,24 @@ export async function generateStaticParams(): Promise<Props["params"][]> {
 }
 
 export default async function PostPage({ params }: Props) {
-  const slug = params?.slug;
-  const project = allProjects.find((project) => project.slug === slug);
+  // const slug = params?.slug;
+  // const project = allProjects.find((project) => project.slug === slug);
 
-  if (!project) {
-    notFound();
-  }
+  // if (!project) {
+  //   notFound();
+  // }
 
-  const views =
-    (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
+  // const views =
+  //   (await redis.get<number>(["pageviews", "projects", slug].join(":"))) ?? 0;
 
   return (
     <div className="bg-zinc-50 min-h-screen">
-      <Header project={project} views={views} />
+      {/* <Header project={project} views={views} />
       <ReportView slug={project.slug} />
 
       <article className="px-4 py-12 mx-auto prose prose-zinc prose-quoteless">
         <Mdx code={project.body.code} />
-      </article>
+      </article> */}
     </div>
   );
 }
